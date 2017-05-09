f7d0be02f83357c0aa556362cffedb5918425979
Travis tang <yiutingtang@gmail.com>
2017-05-10 01:10:10 +0800

    read and write function done in the sandbox

--- sandbox.py
+++ sandbox.py
@@ -20,7 +20,7 @@ def linear_combination(w,M):
 	w = tf.expand_dims(w,1) # batch_size,_,weights
 	x = tf.matmul(w,M) # batch_size,1,num_keys
 	x = tf.squeeze(x,1)# batch_size,num_keys
-	x = tf.reduce_sum(x,0) # batch_size,1
+	#x = tf.reduce_sum(x,0) # batch_size,1
 	return x
 """
 	Input:
@@ -95,21 +95,20 @@ def shift_location(s,w):
 	weight_length = w_shape[-1]
 	w = tf.concat([tf.slice(w,[0,weight_length - pad_length],[batch_size,pad_length]),w,tf.slice(w,[0,0],[batch_size,pad_length])],-1)	
 	""" prepare for conv 2d"""
-	w = tf.expand_dims(w,1) # 1,1,r
-	w = tf.expand_dims(w,-1) # b, 1, r ,1: "Batch",
-	w = tf.expand_dims(w,0) # 1,b,1,r,1: 3d batch, batch,h,w,in_channel
-	s = tf.expand_dims(s,1) # b,1,s
-	s = tf.expand_dims(s,-1) # b,1,s,1
-	s = tf.expand_dims(s,-1) # b,1,s,1,1: Batch,height,width,in_channel,out_channel
-
 	debug_shape(w,'w')
 	debug_shape(s,'s')
+	w = tf.expand_dims(w,1) # b,1,r
+	w = tf.expand_dims(w,-1) # b, 1, r ,1: "Batch",h,w,in_channel
+	s = tf.expand_dims(s,-1) # b,s,1
+	s = tf.expand_dims(s,-1) # b,s,1,1: batch, w,h,out_channel
+	batch_size = w.get_shape().as_list()[0]	
+	ws = tf.split(w,batch_size,0)
+	ss = tf.split(s,batch_size,0)
 	"""conv"""
-	res = tf.nn.conv3d(w,s,[1,1,1,1,1],'VALID')
-	debug_shape(res,'res')
-	exit()
+	res = [tf.nn.conv2d(w,s,[1 for _ in range(4)],'VALID') for (w,s) in zip(ws,ss)]	
+	res = [tf.squeeze(tf.squeeze(r,0),-1) for r in res]
+	res = tf.stack(res,0)
 	res = tf.squeeze(res,1)
-	res = tf.squeeze(res,-1)
 	return res
 """
 	Input:
@@ -121,27 +120,52 @@ def shift_location(s,w):
 def sharpening(g,w):
 	assert_rank(g,2)
 	assert_rank(w,2)
+	key_length = tf.shape(w)[-1]
+	g = tf.tile(g,[1,key_length])
 	res = tf.pow(w,g)
-	return res / tf.reduce_sum(res,-1)
-def read(k,b,g,s,t,M,wt):
+	return tf.nn.l2_normalize(res,-1)
+	#return res / tf.reduce_sum(res,-1)
+
+def get_weight(k,b,g,s,t,M,wt):
 	wc = content_addressing(b,k,M)	
 	wg = interpolate(g,wc,wt)
 	ws = shift_location(s,wg)	
 	w_fin = sharpening(t,ws)
-	w_fin = tf.nn.l2_noramlize(w_fin,-1)
-	return linear_combination(w_fin,M)
+	return w_fin
+	#return linear_combination(w_fin,M)
+
+def read(k,b,g,s,t,M,wt):
+	w = get_weight(k,b,g,s,t,M,wt)
+	return linear_combination(w,M)
+
 def write(k,b,g,s,t,M,wt,e,a):
-	pass
+	e = tf.clip_by_value(e,0,1) # values of e should be restricted to [0,1]
+	assert_rank(e,2) # e should have shape (b,k)
+	assert_rank(a,2) # a should have shape (b,k)
+	w = get_weight(k,b,g,s,t,M,wt) # w has shape (b,n)
+	w = tf.expand_dims(w,-1) # (b,n,1)
+	""" erase """
+	e = tf.expand_dims(e,1) # (b,1,k)
+	e = 1 - tf.matmul(w,e) # (b,n,k)
+	""" add """
+	a = tf.expand_dims(a,1) # (b,1,k)
+	a = tf.matmul(w,a) # (b,n,k)
+	return tf.multiply(M,e) + a # (b,n,k) as new contents of the memory
+
+	
 M = tf.Variable(np.array([[[1,1,1],[2,2,2],[1,1,1],[1,2,1]],[[1,1,1],[2,2,2],[1,1,1],[1,2,1]]]),dtype = tf.float32) # 1,4,3: batch_size,time step,mem_length
 #M = tf.transpose(M,perm = [0,2,1]) # 1,3,4
-wc = tf.Variable([[2,3,4,5],[1,2,3,4]],dtype = tf.float32)
+wc = tf.Variable([[1,1,1,1],[1,1,1,1]],dtype = tf.float32)
 k = tf.Variable([[1,1,1],[1,1,1]],dtype = tf.float32) # 1,3
 b = tf.Variable([[1],[1]],dtype = tf.float32) # 1,1
-g = tf.Variable([[0.5],[0.5]],dtype = tf.float32) #1,1
-s = tf.Variable([[1,0,0],[0,0,1]],dtype = tf.float32) # 1,3
-t = tf.Variable([[0.5],[0.5]],dtype = tf.float32)
+g = tf.Variable([[0],[0]],dtype = tf.float32) #1,1
+s = tf.Variable([[0,1,0],[0,1,0]],dtype = tf.float32) # 1,3
+t = tf.Variable([[1],[1]],dtype = tf.float32)
+e = tf.Variable([[1.0,1.0,1.0],[1.0,1.0,1.0]])
+a = tf.Variable([[1.0,1.0,0.0],[0.0,0.0,0.0]])
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	new_w = read(k,b,g,s,t,M,wc)
-	print sess.run(new_w)
+	#new_w = read(k,b,g,s,t,M,wc)
+	new_M = write(k,b,g,s,t,M,wc,e,a)
+	print sess.run(new_M)[1]
 	
